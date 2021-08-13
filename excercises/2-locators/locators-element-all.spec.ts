26a3d58e09c3f5360bd22ac4fa9c4f080f914317
Krystian Plich <KrystianPlich@github.com>
2021-08-13 13:04:32 +0200

    Task2- Add missing assertions

--- excercises/2-locators/locators-element-all.spec.ts
+++ excercises/2-locators/locators-element-all.spec.ts
@@ -11,8 +11,7 @@ describe('Protractor workshop app', function () {
 	 * to see how get function can be used
 	 */
 	it('should have menu items with links to "Home", "About", "Services", "Blog", "Contact",  pages', async function () {
-		const menuText = ["Home", "About", "Services", "Blog", "Contact"]
-		await browser.get("/jak-to-zrobic-w-js");
+		const menuText = ["Home", "About", "Services", "Blog", "Contact"];
 		var menuItems = element.all(by.css('ul.nav > li > a'));
 		expect(await menuItems.get(0).getText()).toEqual(menuText[0]);
 	});
@@ -22,16 +21,29 @@ describe('Protractor workshop app', function () {
 	* to see how map function can be used to verify content of multiple elements
 	*/
 
-	xit('should have Feature A, Feature B, Feature C sections ...', function () {
+	it('should have Feature A, Feature B, Feature C sections ...', async function () {
 		//TODO: User map function to verify feature names
+		const expectedText = ["Feature A", "Feature B", "Feature C"];
+		let featureText = element.all(by.css('.row.feature-box .span4 h2')).map((item, index) => item.getText());
+		expect(await featureText).toEqual(expectedText);
 	});
 
 	/**
 	 * Check http://angular.github.io/protractor/#/api?view=ElementArrayFinder
 	 * to see how fileter function can be used select elements based on condition
 	 */
-	xit('should route to "Blog" pages after selecting link', function () {
+	it('should route to "Blog" pages after selecting link', async function () {
 		//TODO: User filter function to verify feature names
+		const expectedFeature = "Blog";
+		const expectedTitle = "Protractor workshop | Blog";
+		let blogItem = element.all(by.css('ul.nav li a')).filter((elem) => {
+			return elem.getText().then((text) => {
+				return text === expectedFeature
+			})
+		});
+		await blogItem.click();
+		let pageTitle = browser.getTitle();
+		expect(await pageTitle).toBe(expectedTitle);
 	});
 
 });
