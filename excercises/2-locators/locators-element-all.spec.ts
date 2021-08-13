988716a64d14066dfceda0e2000b67a8a96ffc2e
Krystian Plich <KrystianPlich@github.com>
2021-08-13 14:23:55 +0200

    Task2- Updated minor issues

--- excercises/2-locators/locators-element-all.spec.ts
+++ excercises/2-locators/locators-element-all.spec.ts
@@ -22,10 +22,9 @@ describe('Protractor workshop app', function () {
 	*/
 
 	it('should have Feature A, Feature B, Feature C sections ...', async function () {
-		//TODO: User map function to verify feature names
 		const expectedText = ["Feature A", "Feature B", "Feature C"];
-		let featureText = element.all(by.css('.row.feature-box .span4 h2')).map((item, index) => item.getText());
-		expect(await featureText).toEqual(expectedText);
+		let featureText = await element.all(by.css('.row.feature-box .span4 h2')).map((item) => item.getText());
+		expect(featureText).toEqual(expectedText);
 	});
 
 	/**
@@ -36,14 +35,9 @@ describe('Protractor workshop app', function () {
 		//TODO: User filter function to verify feature names
 		const expectedFeature = "Blog";
 		const expectedTitle = "Protractor workshop | Blog";
-		let blogItem = element.all(by.css('ul.nav li a')).filter((elem) => {
-			return elem.getText().then((text) => {
-				return text === expectedFeature
-			})
-		});
-		await blogItem.click();
-		let pageTitle = browser.getTitle();
-		expect(await pageTitle).toBe(expectedTitle);
+		await element.all(by.css('ul.nav li a')).filter(async (item) => await item.getText() == expectedFeature).click();
+		let pageTitle = await browser.getTitle();
+		expect(pageTitle).toBe(expectedTitle);
 	});
 
 });
